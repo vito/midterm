4da23207913c07fe8e487d93e70d53a8dcc33de3
James Aguilar <aguilar.james@gmail.com>
2015-08-21 10:27:22 -0700

    Add TODO for subpackage for the export code.

--- export.go
+++ export.go
@@ -8,6 +8,10 @@ import (
 // Handler is a type that knows how to serve the VT100 as an HTML
 // page. This is useful as a way to debug problems, or display
 // the state of the terminal to a user.
+//
+// TODO(jaguilar): move me to a subpackage, then export to the
+// default mux automatically. This seems to be the way it's done
+// in the std lib.
 type Handler struct {
 	*VT100
 }
